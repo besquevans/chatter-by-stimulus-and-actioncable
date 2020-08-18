4c3ad84c40ffc86635fe1c7e254801142e6ee34d
Gavin Liu <du20783@gmail.com>
2020-08-18 14:46:14 +0800

    edit timeline channel

--- app/javascript/channels/timeline_channel.js
+++ app/javascript/channels/timeline_channel.js
@@ -1,15 +1,8 @@
-import consumer from "./consumer"
+import CableReady from 'cable_ready';
+import consumer from './consumer';
 
-consumer.subscriptions.create("TimelineChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
+consumer.subscriptions.create('TimelineChannel', {
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
+    if (data.cableReady) CableReady.perform(data.operations);
+  },
 });
